b472e63daf701849e217014ff7f6b80c6e05df0a
wooki <wooki@HowlingPirates>
2019-10-15 18:36:05 -0700

    commit message

--- src/ExtraCredit.java
+++ src/ExtraCredit.java
@@ -13,6 +13,7 @@ public class ExtraCredit {
 
 	public static void main(String[] args) {
 		steeple();
+		topChunk();
 	}
 	
 	public static void steeple() {
@@ -25,14 +26,42 @@ public class ExtraCredit {
 	}
 	
 	public static void topChunk() {
-		for(int i = 0; i < 4; i++) {
-			System.out.print("__/");
-			for(int j = 0; j <= 3*i; j++) {
-				System.out.print(":");
+		for(int lines = 0; lines < 4; lines++) { // this whole for loop prints the sloped portion of the tower
+			for(int i = 9 - lines * 3; i >= 0; i--) { // prints fewer and fewer spaces as lines gets larger
+				System.out.print(" ");
 			}
-			System.out.print("||");
+			System.out.print("__/");
+			for(int j = 0; j <= 3 * lines - 1; j++) { // prints the colins in the "meat" 
+				System.out.print(".");
+			}
+			System.out.print("||"); // prints the center portion
+			for(int k = 0; k < 3 * lines; k++) { // does the same thing as the above for loop
+				System.out.print(".");
+			}
+			System.out.println("\\__");
 			
 		}
+		quoteLine();
+		
+		for(int lines = 0; lines < 4; lines++) { // prints the bottom half of the cafe area
+			for(int i = lines * 2; i >= 0; i--) { // increases number of spaces based on what line is being written
+				System.out.print(" ");
+			}
+			System.out.print("\\_");
+			for(int j = 10 - (2 * lines); j >= 0; j--) {
+				System.out.print("/\\"); // prints the "meat" of the lower half: /\
+			}
+			System.out.println("_/");
+		}
 	}
+	
+	public static void quoteLine() { // this prints the line of quotation marks surrounded by | characters
+		System.out.print(" |");
+		for(int i = 1; i <= 24; i++) {
+			System.out.print("\"");
+		}
+		System.out.println("|");
+	}
+	
 
 }
